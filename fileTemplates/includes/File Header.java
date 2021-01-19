f9b50ed46183a814efa48ee928ac38c041af4bc9
liuzichen <liuzichen@eigpay.com>
2021-01-19 08:53:55 +0800

    IU-2020.1.1 <panhaijiao@haijiaodeMBP.lan Create markdown.xml, p3c.xml, project.default.xml, diff.xml, Enum.java, Interface.java, Default.xml, csv-plugin.xml, file.template.settings.xml, ignore.xml, find.xml, File Header.java, androidEditors.xml, KeyPromoterXSettings.xml, path.macros.xml, mavenVersion.xml, javarebel.xml, Record.java, GrepConsole.xml, Default.xml, workspace.xml, MyBatisCodeHelper.xml, keymap.xml, OGNL.xml, editor.xml, User Tempplate.xml, debugger.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, ide.general.xml, CodeGlance.xml, rainbow_brackets.xml, macOS copy.xml, lombok-plugin.xml, user.xml, git.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, mygroup.xml, VisualVMLauncher.xml, databaseDrivers.xml, templates.xml, AnnotationType.java, stringManipulation.xml, studioFlags.xml, OGNL _Struts 2_.xml, parameter.hints.xml, Class.java, jdk.table.xml, mavenRunHelper.xml, intentionSettings.xml, laf.xml, Eclipse _Mac OS X_ copy.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, mybatis.xml, usageView.xml, sonarlint.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,5 @@
+/**
+ * 
+ * @author liuzc
+ * @version ${NAME}.java, v 0.1 ${DATE} ${TIME} liuzc Exp 初始化创建
+ */
---
+++ fileTemplates/internal/AnnotationType.java
@@ -0,0 +1,8 @@
+/*
+  Eigpay.com Inc.
+  Copyright (c) 2013-${YEAR} All Rights Reserved.
+ */
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public @interface ${NAME} {
+}
